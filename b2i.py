6425d316b31409fe34444c463ab21fd9c3e137fb
unknown <bradms98@gmail.com>
2019-08-22 01:13:32 -0500

    added bindInput

--- b2i.py
+++ b2i.py
@@ -1,5 +1,7 @@
 class b2i():
     def __init__(self, bindInput):
+        self.bindInput = bindInput
+        
         if not bindInput:
             self.bindInput = 'Default Input'
         self.csvOutput = bindInput + ' ' + bindInput
