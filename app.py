1461498a4dd565a7b2fc77c00b73ec0a994500d7
Engberg <brad.engberg@servicemaster.com>
2019-08-26 14:19:44 -0500

    Cleanup

--- app.py
+++ app.py
@@ -6,21 +6,6 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '23eb28c4c8bc3cafa6faf2968fa6f322'
 
-posts = [
-    {
-        'author': 'Brad Engberg',
-        'title': 'Using Templates',
-        'content': 'This is the content',
-        'date': '3/20/1980',
-    },
-    {
-        'author': 'Brad Engberg',
-        'title': 'Title2',
-        'content': 'This is more content',
-        'date': '3/20/1981',
-    }
-]
-
 bindInput = 'this is the input text'
 
 @app.route('/', methods=['GET', 'POST'])
@@ -42,43 +27,6 @@ def home():
                      "attachment; filename=" + converted.zone + ".csv"})
     else: 
         return render_template('home.html', form=form, output=output, title='Home')
-
-
-
-
-
-
-
-
-
-
-@app.route('/about')
-@app.route('/about/')
-def about():
-    return render_template('about.html', title='About')
-
-@app.route('/hello/<name>')
-def hello(name):
-    return render_template('name.html', name=name)
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        flash('Account Created for ' + form.username.data + '!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Login unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form) 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
